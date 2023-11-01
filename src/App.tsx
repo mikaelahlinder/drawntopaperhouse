40367b1f032ccd16dbf1c1af3d1d4bcba914a3f9
Mikael Ahlinder <mikael.ahlinder@akindgroup.com>
2023-11-01 19:33:18 +0100

    react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  background-color: #eeede9;
+`
+
+function App() {
+  return <Container>drawn to</Container>
+}
+
+export default App
